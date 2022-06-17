383c10c5a4747d6ac005ac6046f43c6aa922153d
UmarovIkrom <ikromme13@gmail.com>
2022-06-17 17:13:02 +0500

    header section Created & RWD

---
+++ script.js
@@ -0,0 +1,8 @@
+// "use strict";
+
+// const toggleBtn = documents.getElementsByClassName('menu-btn')[0]
+// const navbarLinks = documents.getElementsByClassName('nav__links')[0]
+
+// toggleBtn.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active')
+// });

8e6cc27a999d8d3bdf164604802a29bf6a877793
UmarovIkrom <ikromme13@gmail.com>
2022-06-18 23:07:52 +0500

    modal add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 // const toggleBtn = documents.getElementsByClassName('menu-btn')[0]
 // const navbarLinks = documents.getElementsByClassName('nav__links')[0]
@@ -6,3 +6,33 @@
 // toggleBtn.addEventListener('click', () => {
 //     navbarLinks.classList.toggle('active')
 // });
+
+///////////////////////////////////////
+// Modal window
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+    // e.peventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});

2f1ee2cd6d2fe929efa1b5d13a4de43d749d68d5
UmarovIkrom <ikromme13@gmail.com>
2022-06-20 22:50:01 +0500

    smooth scrolling add to Js file

--- script.js
+++ script.js
@@ -13,6 +13,13 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 
 const openModal = function (e) {
     // e.peventDefault();
@@ -34,5 +41,21 @@ overlay.addEventListener('click', closeModal);
 document.addEventListener('keydown', function (e) {
     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
         closeModal();
+    }
+});
+
+///////////////////////////////////
+// Scrolling
+btnScrollTo.addEventListener('click', function (e) {    
+    section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+        const id = e.target.getAttribute('href');
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'});
     }
 });
